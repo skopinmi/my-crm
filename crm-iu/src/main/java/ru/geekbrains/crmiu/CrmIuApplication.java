f83967f473df55f961e1257d696f8a542333b819
Skopin <Skopin_MI@mail.ru>
2021-02-18 12:36:14 +0300

    init

---
+++ crm-iu/src/main/java/ru/geekbrains/crmiu/CrmIuApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.crmiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmIuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmIuApplication.class, args);
+    }
+
+}
---
+++ crm-iu/src/test/java/ru/geekbrains/crmiu/CrmIuApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.crmiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmIuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
